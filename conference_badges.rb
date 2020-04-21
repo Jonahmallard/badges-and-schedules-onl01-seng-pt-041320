eaafe9c7d92b6f902ba37a2a2c8cdf22e2ca5f67
Jonah Mallard <benjaminjmallard@gmail.com>
2020-04-21 22:16:24 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,14 +4,11 @@ end
 
 badge_maker("Arel")
 
-`def batch_badge_creator(attendees_array)
-  "Hello, my name is #{attendees_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"].each do |name| puts name end}."
+
+def batch_badge_creator(attendees_array)
+      new_array = []
+        attendees_array.each do |name| 
+          new_array<< "Hello, my name is #{name}."
+        end
+  new_array
 end
-
-
-
-
-
-# attendees_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-#batch_badge_creator()
